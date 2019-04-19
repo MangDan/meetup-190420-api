f5198c1d02ab1b934dc242e674237fd7376a36a9
mangdan <dan.donghu.kim@gmail.com>
2019-04-19 17:55:59 +0900

    meetup

--- meeptup-190420-helidon-demo-mp/src/main/java/io/helidon/examples/quickstart/GreetResource.java
+++ meeptup-190420-helidon-demo-mp/src/main/java/io/helidon/examples/quickstart/GreetResource.java
@@ -1,19 +1,3 @@
-/*
- * Copyright (c) 2018, 2019 Oracle and/or its affiliates. All rights reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package io.helidon.examples.quickstart;
 
 import java.util.Collections;
@@ -34,19 +18,10 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.eclipse.microprofile.metrics.MetricUnits;
+import org.eclipse.microprofile.metrics.annotation.Counted;
+import org.eclipse.microprofile.metrics.annotation.Timed;
 
-/**
- * A simple JAX-RS resource to g 
- * Get default greeting message:
- * curl -X GET http://localhost: 
- * Get greeting message for Joe:
- * curl -X GET htt 
- * 
- * Change greeting
- * curl -X PUT -H "Content-Type: application/json" -d '{"greeting" : "Howdy"}' http://localhost:8080/greet/greeting
- *
- * The message is returned as a JSON object.
- */
 @Path("/greet")
 @RequestScoped
 public class GreetResource {
@@ -55,18 +30,10 @@ public class GreetResource {
 
     private static final Logger logger = Logger.getLogger("io.helidon.microprofile.helloworld.level");
 
-    /**
-     * The greeting message provider.
-     */
     private final GreetingProvider greetingProvider;
     private final String appName;
     private FaultTolerance faultTolerance;
-    /**
-     * Using constructor injection to get a configuration property.
-     * By default this gets the value from META-INF/microprofile-config
-     *
-     * @param greetingConfig the configured greeting message
-     */
+    
     @Inject
     public GreetResource(GreetingProvider greetingConfig, FaultTolerance faultTolerance, @ConfigProperty(name = "app.name") String appName) {
         this.greetingProvider = greetingConfig;
@@ -74,12 +41,6 @@ public class GreetResource {
         this.faultTolerance = faultTolerance;
     }
 
-    /**
-     * Return a wordly greeting message.
-     *
-     * @return {@link JsonObject}
-     */
-    @SuppressWarnings("checkstyle:designforextension")
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public JsonObject getDefaultMessage() {
@@ -87,13 +48,18 @@ public class GreetResource {
         return createResponse("World");
     }
 
-    /**
-     * Return a greeting message using the name that was provided.
-     *
-     * @param name the name to greet
-     * @return {@link JsonObject}
-     */
-    @SuppressWarnings("checkstyle:designforextension")
+    @Counted(
+        name = "accessctrDefault",
+        reusable = true,
+        description = "Total greetings accesses",
+        displayName = "Access Counter",
+        monotonic = true,
+        unit = MetricUnits.NONE)
+    @Timed(name = "accessctrTimer",
+        reusable = true,
+        description = "Timer greetings accesses",
+        displayName = "Timer Counter",
+        unit = MetricUnits.SECONDS)
     @Path("/{name}")
     @GET
     @Produces(MediaType.APPLICATION_JSON)
@@ -101,13 +67,6 @@ public class GreetResource {
         return createResponse(name);
     }
 
-    /**
-     * Set the greeting to use in future messages.
-     *
-     * @param jsonObject JSON containing the new greeting
-     * @return {@link Response}
-     */
-    @SuppressWarnings("checkstyle:designforextension")
     @Path("/greeting")
     @PUT
     @Consumes(MediaType.APPLICATION_JSON)
@@ -127,7 +86,6 @@ public class GreetResource {
         return Response.status(Response.Status.NO_CONTENT).build();
     }
 
-    @SuppressWarnings("checkstyle:designforextension")
     @Path("/fault-tolerance")
     @GET
     @Produces(MediaType.APPLICATION_JSON)
